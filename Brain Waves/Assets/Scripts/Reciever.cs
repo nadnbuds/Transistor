e13d6effbe2c153ff4e1b079da8435bef432036d
rwern002 <rwern002@ucr.edu>
2018-01-27 01:08:34 -0800

    Basic On Trigger Enter function that uses Resource as a component of a game object

---
+++ Brain Waves/Assets/Scripts/Reciever.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DVital : MonoBehaviour
+{
+    void Values ()
+    {
+
+    }
+
+    void Increment ()
+    {
+
+    }
+
+    void Decrement ()
+    {
+
+    }
+
+    void OnZero ()
+    {
+
+    }
+
+
+}
+public class Reciever : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        int count = 0;
+        if (other.gameObject.GetComponent<Resource>)
+        {
+            //increase vital that reciever relates to
+            other.GetComponent<Renderer>.enabled = false;
+            ++count;
+        }
+    }
+}
