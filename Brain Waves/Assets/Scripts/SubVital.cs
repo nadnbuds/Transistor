8166006c9f0dde62cccb546a81773ed680de86d0
JBeto <jbeto001@ucr.edu>
2018-01-27 10:25:35 -0800

    Added plugins and game manager

---
+++ Brain Waves/Assets/Scripts/SubVital.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SubVital : Vital
+{
+    private IEnumerable<Vital> vitals;
+
+    public SubVital(IEnumerable<Vital> effectedVitals)
+    {
+        this.vitals = effectedVitals;
+    }
+
+    public override abstract void Increment();
+    public override abstract void Decrement();
+}
