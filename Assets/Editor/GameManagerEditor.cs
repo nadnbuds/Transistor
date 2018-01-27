ed0322ce0bbd42fd127a63096ce5341643bcddf4
JBeto <jbeto001@ucr.edu>
2018-01-27 10:51:21 -0800

    Added Game manager Editor

---
+++ Assets/Editor/GameManagerEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+
+[CustomEditor(typeof(GameManager))]
+public class GameManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+    }
+}
