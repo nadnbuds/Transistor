834265ea6ddee9cdb8725a5229549d643f6f63c4
ychen133 <ychen133@ucr.edu>
2018-01-27 12:49:40 -0800

    Made some very basic VitalResource prefabs and added weighted percentage in AlternatingProducer

--- Assets/Scripts/Producers/AlternatingProducer.cs
+++ Assets/Scripts/Producers/AlternatingProducer.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Web.UI;	//should give me the library for Pair but it doesn't. Why?
 
 public class AlternatingProducer : Producer {
 
-	public VitalResource resource2;
-	public int resource_switch = 0;
+	//public VitalResource resource2;
+
+	//public int resource_switch = 0;
+
+	[SerializeField]
+	private List<KeyValuePair<VitalResource, float>> resource_list;
+	//private List<Pair<ResourceType, float>> resource_list;
 
 	protected override void generate(){
-		if (resource_switch == 0) {
-			Instantiate (resource, randDrop (), Quaternion.identity);
-			resource_switch = 1;		
-		} else {
-			Instantiate (resource2, randDrop (), Quaternion.identity);
-			resource_switch = 0;
+		float sum = 0;
+		foreach(KeyValuePair<VitalResource, float> pair in resource_list){
+			sum += pair.Value;
+		}
+		float random = Random.Range (0, sum);
+		float prev_float = 0;
+		foreach(KeyValuePair<VitalResource , float> pair in resource_list){
+			if (random >= prev_float && random < prev_float + pair.Value) {
+				Instantiate (pair.Key, transform.position + randDrop (), Quaternion.identity);
+				return;
+			}
+			prev_float = prev_float + pair.Value;
 		}
 	}
 
