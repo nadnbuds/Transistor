f8d89631916c5a7f7a7950b8ce09aaa7d8555914
ychen133 <ychen133@ucr.edu>
2018-01-27 11:00:22 -0800

    Updated AlternatingProducer

--- Assets/Scripts/Producers/AlternatingProducer.cs
+++ Assets/Scripts/Producers/AlternatingProducer.cs
@@ -2,7 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AlternatingProducer : MonoBehaviour {
+public class AlternatingProducer : Producer {
+
+	public VitalResource resource2;
+	public bool resource_switch = 0;
+
+	protected override void generate(){
+		if (resource_switch == 0) {
+			Instantiate (resource, randDrop (), Quaternion.identity);
+			resource_switch = 1;		
+		} else {
+			Instantiate (resource2, randDrop (), Quaternion.identity);
+			resource_switch = 0;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +24,6 @@ public class AlternatingProducer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//generate ();
 	}
 }
