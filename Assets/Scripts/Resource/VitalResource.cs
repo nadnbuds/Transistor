26e83bd5bf44936c579f7bb0d819a2d4dff35f79
nadnbuds <lalam003@ucr.edu>
2018-01-27 11:24:30 -0800

    Implemented Treadmill Producer

--- Assets/Scripts/Resource/VitalResource.cs
+++ Assets/Scripts/Resource/VitalResource.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public enum ResourceType { Heart, Breathing, Digestion, Thought, Heat, All, MAX }
 
-public abstract class VitalResource : MonoBehaviour
+public abstract class VitalResource : MonoBehaviour, Interactable
 {
     [SerializeField]
     protected ResourceType resourceType;
     [SerializeField]
     protected int quantity;
+    [SerializeField]
+    protected float dropForce = 3;
 
     public ResourceType Type { get; protected set; }
     public int Quantity { get; protected set; }
-    
+
+    public abstract void ToggleInteract(PlayerController player);
+
     protected void Init()
     {
         Type = resourceType;
