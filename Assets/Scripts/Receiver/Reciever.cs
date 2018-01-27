69fe50a27dd0b691058340edee59a1d3e2fea14c
nadnbuds <lalam003@ucr.edu>
2018-01-27 13:36:10 -0800

    Reciever and Meta files

--- Assets/Scripts/Receiver/Reciever.cs
+++ Assets/Scripts/Receiver/Reciever.cs
@@ -6,24 +6,16 @@ public class Reciever : MonoBehaviour
 {
     [SerializeField]
     private Vital vital;
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnTriggerEnter(Collider other)
     {
         VitalResource resource;
         if (resource = other.gameObject.GetComponent<VitalResource>())
         {
-            //increase vital that reciever relates to
-            other.gameObject.SetActive(false);
-            vital.Health += resource.Quantity;
+            if (vital.ParseResource(resource))
+            {
+                other.gameObject.SetActive(false);
+            }
         }
     }
 }
