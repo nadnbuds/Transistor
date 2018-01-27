1d669a57c55a96740385d66e4d3155077eb3cf23
Rica Feng <rfeng001@ucr.edu>
2018-01-27 15:12:30 -0800

    Vitlal front end display with Modify method

---
+++ Assets/Scripts/Vitals/VitalDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VitalDisplay : MonoBehaviour
+{
+    /// <summary>
+    /// Max fill height
+    /// </summary>
+    private float GetMaxFill;
+
+    /// <summary>
+    /// This rect transform
+    /// </summary>
+    private RectTransform rt;
+
+    private void Awake()
+    {
+        rt = GetComponent<RectTransform>();
+        GetMaxFill = GetComponentInParent<RectTransform>().rect.height;
+    }   
+
+    /// <summary>
+    /// Set fill
+    /// </summary>
+    public void ModifyPercentage(float p)
+    {
+        float newFill = p * GetMaxFill;
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x, newFill);
+    }
+}
