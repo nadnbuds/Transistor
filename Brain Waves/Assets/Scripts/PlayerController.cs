d850b63fe05e8d1114d07d6caae244645ee27e19
nadnbuds <lalam003@ucr.edu>
2018-01-27 01:41:33 -0800

    Player Controller -Added User Input -Added Player Controller

---
+++ Brain Waves/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float interactionDistance;
+
+    private bool canPickup = true;
+    private Resource focusObj;
+
+	public void Move(float horizontal, float vertical)
+    {
+        Vector3 newVelocity = new Vector3(
+            horizontal * moveSpeed,
+            gameObject.GetComponent<Rigidbody>().velocity.y, // or 0
+            vertical * moveSpeed);
+
+        this.gameObject.GetComponent<Rigidbody>().velocity = newVelocity;
+    }
+
+    public void Action()
+    {
+        if (canPickup)
+            pickupObj();
+        else
+            putDownObj();
+    }
+
+    private void pickupObj()
+    {
+        focusObj.transform.SetParent(transform);
+        canPickup = false;
+    }
+
+    private void putDownObj()
+    {
+        focusObj.transform.SetParent(null);
+        canPickup = true;
+    }
+
+    private void Update()
+    {
+        if (canPickup)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance))
+            {
+                if (hit.GetType() == typeof(Resource))
+                {
+                    focusObj = hit.transform.gameObject as Resource;
+                }
+            }
+        }
+    }
+}
---
+++ Brain Waves/Assets/Scripts/UserInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class UserInput : MonoBehaviour {
+
+    [SerializeField]
+    private PlayerController player;
+
+    private float horizontal, vertical;
+
+    private bool action;
+
+	private void Update () {
+        if (Input.GetButtonDown("Space"))
+            action = true;
+	}
+
+    private void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        player.Move(horizontal, vertical);
+        if (action)
+        {
+            player.Action();
+            action = false;
+        }
+        
+    }
+}
