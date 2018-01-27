25de04440d76b97be07021cbde87d5fbe8185263
Rica Feng <rfeng001@ucr.edu>
2018-01-27 15:38:50 -0800

    framework for vital display status

--- Assets/Scripts/Vitals/VitalDisplay.cs
+++ Assets/Scripts/Vitals/VitalDisplay.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VitalDisplay : MonoBehaviour
 {
+    [SerializeField]
+    public Image vitalStatus;
+
+    [Space(10)]
+
+    [SerializeField]
+    public Image decreaseStatus;
+
+    [SerializeField]
+    public Image veryDecreaseStatus;
+
+    [SerializeField]
+    public Image increaseStatus;
+
     /// <summary>
     /// Max fill height
     /// </summary>
     private float GetMaxFill;
+
+    [Range(0f, 1f)]
+    private float currentPercentage;
 
     /// <summary>
     /// This rect transform
@@ -18,6 +36,7 @@ public class VitalDisplay : MonoBehaviour
     {
         rt = GetComponent<RectTransform>();
         GetMaxFill = GetComponentInParent<RectTransform>().rect.height;
+        currentPercentage = GetComponent<RectTransform>().rect.height / GetMaxFill;
     }   
 
     /// <summary>
@@ -27,5 +46,17 @@ public class VitalDisplay : MonoBehaviour
     {
         float newFill = p * GetMaxFill;
         rt.sizeDelta = new Vector2(rt.sizeDelta.x, newFill);
+
+        float diff = currentPercentage - p;
+        if (diff > 0)
+        {
+            //Increase
+            vitalStatus = increaseStatus;
+        }
+        else
+        {
+            //Lower
+            vitalStatus = decreaseStatus;
+        }
     }
 }
